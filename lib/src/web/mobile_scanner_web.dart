2ce825dd32dd8afe450b3a1a524614f4237b9bc4
buchta <buchta@smartman.sk>
2024-11-21 13:59:26 +0100

    - remove logging

--- lib/src/web/mobile_scanner_web.dart
+++ lib/src/web/mobile_scanner_web.dart
@@ -17,7 +17,7 @@ import 'package:mobile_scanner/src/web/barcode_reader.dart';
 import 'package:mobile_scanner/src/web/media_track_extension.dart';
 import 'package:mobile_scanner/src/web/zxing/zxing_barcode_reader.dart';
 import 'package:web/web.dart';
-import 'package:mobile_scanner/src/web/javascript_map.dart';
+import 'package:collection/collection.dart';
 
 /// A web implementation of the MobileScannerPlatform of the MobileScanner plugin.
 class MobileScannerWeb extends MobileScannerPlatform {
@@ -177,20 +177,33 @@ class MobileScannerWeb extends MobileScannerPlatform {
       final List<MediaStreamTrack>? tracks = mediaStream?.getVideoTracks().toDart;
       tracks?.forEach((track) => track.stop());
 
-      // String preferredDeviceId = "";
-      // try {
-      //   final availableDeviceJs = await window.navigator.mediaDevices.enumerateDevices().toDart;
-      //   final List<MediaDeviceInfo> availableDeviceDart = availableDeviceJs.toDart;
-      //   // https://www.reddit.com/r/javascript/comments/8eg8w5/choosing_cameras_in_javascript_with_the/
-      //   // The ONLY consistent way I've found to choose an environment-facing "normal" camera in 100% of cases is to call enumerateDevices and choose the LAST item.
-      //   // Now that is not coded into the spec at all, but in all of my testing (over almost 80 different devices) that is ALWAYS the environment-facing "normal" camera.
-      //   // As always your results may vary.
-      //
-      //   preferredDeviceId =
-      //       availableDeviceDart.where((element) => element.kind == "videoinput").last.deviceId as String;
-      // } catch (err) {
-      //   preferredDeviceId = "";
-      // }
+      String? preferredDeviceId;
+      try {
+        final availableDeviceJs = await window.navigator.mediaDevices.enumerateDevices().toDart;
+        final List<MediaDeviceInfo> availableDeviceDart = availableDeviceJs.toDart;
+        // https://www.reddit.com/r/javascript/comments/8eg8w5/choosing_cameras_in_javascript_with_the/
+        // The ONLY consistent way I've found to choose an environment-facing "normal" camera in 100% of cases is to call enumerateDevices and choose the LAST item.
+        // Now that is not coded into the spec at all, but in all of my testing (over almost 80 different devices) that is ALWAYS the environment-facing "normal" camera.
+        // As always your results may vary.
+
+        final List<String> deviceNames = [
+          'Hátoldali kamera',
+          'Zadná kamera',
+          'Back Camera',
+        ];
+
+        final List<MediaDeviceInfo> filteredDevices =
+            availableDeviceDart.where((element) => element.kind == "videoinput").toList();
+
+        preferredDeviceId =
+            filteredDevices.firstWhereOrNull((element) => deviceNames.contains(element.label))?.deviceId;
+
+        if (preferredDeviceId == null || preferredDeviceId.isEmpty) {
+          preferredDeviceId = filteredDevices.last.deviceId;
+        }
+      } catch (err) {
+        preferredDeviceId = "";
+      }
 
       // Check if browser supports multiple camera's and set if supported
       final MediaTrackSupportedConstraints capabilities = window.navigator.mediaDevices.getSupportedConstraints();
@@ -208,26 +221,19 @@ class MobileScannerWeb extends MobileScannerPlatform {
           ),
         );
 
-        // final MediaStreamConstraints constraints = defaultConstraints;
-        //
-        // if (preferredDeviceId.isNotEmpty) {
-        //   constraints = MediaStreamConstraints(
-        //     video: MediaTrackConstraintSet(
-        //       facingMode: facingMode.toJS,
-        //       width: 1280.toJS,
-        //       deviceId: ConstrainDOMStringParameters(
-        //         ideal: preferredDeviceId.toJS,
-        //       ),
-        //     ),
-        //   );
-        // }
+        MediaStreamConstraints constraints = defaultConstraints;
 
-        final MediaStreamConstraints constraints = MediaStreamConstraints(
-          video: MediaTrackConstraintSet(
-            facingMode: facingMode.toJS,
-            width: 1280.toJS,
-          ),
-        );
+        if (preferredDeviceId != null && preferredDeviceId.isNotEmpty) {
+          constraints = MediaStreamConstraints(
+            video: MediaTrackConstraintSet(
+              facingMode: facingMode.toJS,
+              width: 1280.toJS,
+              deviceId: ConstrainDOMStringParameters(
+                ideal: preferredDeviceId.toJS,
+              ),
+            ),
+          );
+        }
 
         try {
           localStream = await window.navigator.mediaDevices.getUserMedia(constraints).toDart;
